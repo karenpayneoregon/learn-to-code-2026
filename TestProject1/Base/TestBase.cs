13bf981b8384459e1d3526182596de1af2f06e64
Karen Payne <paynekaren@comcast.net>
2026-03-02 07:58:57 -0800

    Add TestProject1: MSTest/Shouldly unit test setup
    Introduce TestProject1 for unit testing with MSTest and Shouldly.
    Update solution to include and organize the new test project.
    Add tests for HumanGenerator, custom trait attributes, and
    parallel test execution support. Includes base test class
    and trait categorization for improved test organization.
    
    by Karen

---
+++ TestProject1/Base/TestBase.cs
@@ -0,0 +1,18 @@
+﻿namespace TestProject1.Base
+{
+    public class TestBase
+    {
+        protected TestContext TestContextInstance;
+        public TestContext TestContext
+        {
+            get => TestContextInstance;
+            set
+            {
+                TestContextInstance = value;
+                TestResults.Add(TestContext);
+            }
+        }
+
+        public static IList<TestContext> TestResults;
+    }
+}
---
+++ TestProject1/Base/TestTraitsAttribute.cs
@@ -0,0 +1,42 @@
+﻿namespace TestProject1.Base
+{
+    public enum Trait
+    {
+        /// <summary>
+        /// Place holder 
+        /// </summary>
+        PlaceHolder,
+        Bogus,
+        BogusShouldly
+    }
+    /// <summary>
+    /// Declarative class for using Trait enum about for traits on test method.
+    /// </summary>
+    public class TestTraitsAttribute : TestCategoryBaseAttribute
+    {
+        private readonly Trait[] _traits;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestTraitsAttribute"/> class with the specified traits.
+        /// </summary>
+        /// <param name="traits">An array of <see cref="Trait"/> values representing the traits to associate with the test method.</param>
+        public TestTraitsAttribute(params Trait[] traits)
+        {
+            _traits = traits;
+        }
+
+        /// <summary>
+        /// Gets the list of test categories associated with the traits specified in the attribute.
+        /// </summary>
+        /// <remarks>
+        /// The test categories are derived from the <see cref="Trait"/> enumeration values provided
+        /// when the <see cref="TestTraitsAttribute"/> is applied.
+        /// </remarks>
+        /// <value>
+        /// A list of strings representing the names of the test categories.
+        /// </value>
+        public override IList<string> TestCategories 
+            => _traits.Select(trait => Enum.GetName(typeof(Trait), trait)).ToList();
+    }
+
+}
