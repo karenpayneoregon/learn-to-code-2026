7fe4ca22af14172f4b45db7ea9208641fac88736
Karen Payne <paynekaren@comcast.net>
2026-03-04 13:07:46 -0800

    copy type

---
+++ BasicObjectsExamples/Classes/Simple/FileOperationsStatic.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicObjectsExamples.Classes.Simple;
+
+public class FileOperationsStatic
+{
+
+    public string FolderName => "C:\\Files\\SomeFile.txt";
+
+    public void ReadFile(string fileName)
+    {
+        Console.WriteLine(Path.Combine(FolderName, fileName));
+    }
+}
