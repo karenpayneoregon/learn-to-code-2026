909be85f406c5fed5ac6d5ce81631bf72fe87981
Karen Payne <paynekaren@comcast.net>
2026-03-04 14:47:42 -0800

    new project

---
+++ GeneratorHumansSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using BogusLibrary.Classes;
+
+namespace GeneratorHumansSample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var humans = HumanGenerator.Create(20);
+
+        Debugger.Break();
+        
+        Console.ReadLine();
+    }
+}
