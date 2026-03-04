28b3ebeb9db90b77f9f2cf8fb5897d936f65cb94
Karen Payne <paynekaren@comcast.net>
2026-03-04 13:04:36 -0800

    copy type

---
+++ BasicObjectsExamples/Classes/Simple/FileOperationsStandard.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicObjectsExamples.Classes.Simple;
+
+public class FileOperationsStandard
+{
+    public FileOperationsStandard(string folderName)
+    {
+        FolderName = folderName;
+    }
+    public string FolderName { get; set; }
+
+    public void ReadFile(string fileName)
+    {
+        Console.WriteLine(Path.Combine(FolderName, fileName));
+    }
+}
