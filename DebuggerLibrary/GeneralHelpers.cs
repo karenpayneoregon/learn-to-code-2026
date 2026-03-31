e4a4ee11ddee6d2c5dd0ffa98c5850146fd7c02a
Karen Payne <paynekaren@comcast.net>
2026-03-31 12:48:40 -0700

    Suppress warnings in csproj, add file I/O example method
    Updated DebuggerLibrary.csproj to suppress warnings 1701, 1702, and CS8600 for both Debug and Release builds. Added Example1 static method to GeneralHelpers.cs to demonstrate file creation, appending, and reading. Disabled ReSharper's "ConvertToUsingDeclaration" suggestion.
    
    by Karen

--- DebuggerLibrary/GeneralHelpers.cs
+++ DebuggerLibrary/GeneralHelpers.cs
@@ -1,6 +1,41 @@
-﻿namespace DebuggerLibrary;
+﻿// ReSharper disable ConvertToUsingDeclaration
+namespace DebuggerLibrary;
 
 public class GeneralHelpers
 {
+    public static void Example1()
+    {
+        string path = @"c:\temp\MyTest.txt";
+        // This text is added only once to the file.
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+        }
 
+        // This text is always added, making the file longer over time
+        // if it is not deleted.
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine("This");
+            sw.WriteLine("is Extra");
+            sw.WriteLine("Text");
+        }
+
+        // Open the file to read from.
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
 }
