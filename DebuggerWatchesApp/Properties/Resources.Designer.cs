f61700ec4cee51dd458fa27615fc1432ec732589
Karen Payne <paynekaren@comcast.net>
2026-04-01 13:42:38 -0700

    Add question dialog utility and error dialog support
    - Added a customizable question dialog to the Dialogs class
    - Introduced DisplayError method for exception dialogs
    - Improved XML documentation for Dialogs utilities
    - Refactored BytesToBitmap for clarity
    - Added question32 icon resource and updated resource files
    
    by Karen

--- DebuggerWatchesApp/Properties/Resources.Designer.cs
+++ DebuggerWatchesApp/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace DebuggerWatchesApp.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon question32 {
+            get {
+                object obj = ResourceManager.GetObject("question32", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
